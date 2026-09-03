7067c0b8164c9a3a8d196d019c9b2c08d34681cf
Sonu Kumar <140630062+Sonucse12345@users.noreply.github.com>
2026-09-04 01:02:31 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        diff = {}#number and index
+        for i, val in enumerate(nums):
+            complement = target- val
+            if complement in diff:
+                return [diff[complement],i]
+            diff[val] = i #"Store the current value val and remember that it was at index i."
+
+
